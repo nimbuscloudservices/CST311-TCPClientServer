3549891e43dad2f874e1a7bf2008e1b7c7c67d18
Layla Gallez <layla.gallez@gmail.com>
2022-05-27 23:42:06 -0700

    clients connect to server

--- TCPServer.py
+++ TCPServer.py
@@ -4,33 +4,33 @@ from _thread import *
 
 ServerSocket = socket.socket()
 serverName = '10.0.0.3'
-serverPort = 12000
-serverPort2 = 12001
+serverPort = 12013
 ThreadCount = 0
+
+print('Waitiing for 2 Connections..')
 try:
     ServerSocket.bind((serverName, serverPort))
-    ServerSocket.bind((serverName, serverPort2))
 except socket.error as e:
     print(str(e))
 
-print('Waitiing for a Connection..')
 ServerSocket.listen(2)
 
 
 def threaded_client(connection):
-    connection.send(str.encode('Server Connected'))
+    connection.send(str.encode('Server Connected: ' ))
     while True:
-        data = connection.recv(2048)
-        reply = 'Server: ' + data
-        if not data:
-            break
-        connection.sendall(str.encode(reply))
+        reply = connection.recv(2048).decode()    
+        connection.send(reply.encode())  
     connection.close()
 
 while True:
     client, address = ServerSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
     start_new_thread(threaded_client, (client, ))
+    
     ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
+    
+    print('Accepted first connection, calling it client: ', ThreadCount)
+
 ServerSocket.close()
+
+
